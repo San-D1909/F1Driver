67102805de75c838e5e4b8806df95d3bc8c5a3f5
Sander Aarts <sanderaarts19992@gmail.com>
2021-10-07 12:48:04 +0200

    Homepage changes

--- F1Driver/F1Driver-Front/ClientApp/src/components/Home.js
+++ F1Driver/F1Driver-Front/ClientApp/src/components/Home.js
@@ -9,9 +9,9 @@ export class Home extends Component {
                 <h1>Welcome</h1>
                 <p>This website is currently under devolopment, to see current F1 news go to:</p>
                 <ul>
-                    <li><a href='https://www.formula1.com/'>Official F1 website</a> </li>
-                    <iframe src="https://giphy.com/embed/342Zsv5S4W8XC" width="480" height="252"></iframe>
+                    <a href='https://www.formula1.com/'>Official F1 website</a>
                 </ul>
+                    <iframe src="https://giphy.com/embed/342Zsv5S4W8XC" width="480" height="252"></iframe>
             </div>
         );
     }
